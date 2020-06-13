a75e7e6b7bc9b41fa627e2a6116dfcc68cb5902f
Raphael Albern <ralpha19@hotmail.com>
2020-06-13 20:46:24 +0000

    wrote JOIN queries

--- migrations/assignments_submissions.sql
+++ migrations/assignments_submissions.sql
@@ -13,3 +13,25 @@ CREATE TABLE assignment_submissions ( id SERIAL PRIMARY KEY NOT NULL,
                                       duration INTEGER,
                                       submission_date DATE
                                     );
+
+
+-- SELECT students.name as student_name, students.start_date as student_start_date, cohorts.name as cohort_name, cohorts.start_date as cohort_start_date
+-- FROM students JOIN cohorts ON cohort_id = cohorts.id
+-- WHERE cohorts.start_date != students.start_date
+-- ORDER BY cohort_start_date;
+
+
+-- An assignment will have the following attributes:
+--   id: A unique identifier
+--   name: The name of the assignment
+--   content: The written content body of the assignment
+--   day: The day that the assignment appears on
+--   chapter: The order that the assignment will appear in the day.
+--   duration: The average time it takes a student to finish
+
+-- An assignment_submission will have the following attributes:
+--   id: A unique identifier
+--   assignment_id: The id of the assignment
+--   student_id: The id of the student
+--   duration: The time it took the student to complete the assignment
+--   submission_date: The date is was submitted
