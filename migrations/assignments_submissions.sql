85d39abd9f700b264f717a2b9c8415426b2f8e62
Raphael Albern <ralpha19@hotmail.com>
2020-06-15 23:11:07 +0000

    created teachers and assistance_requests table and created ERD and seeds data into the tables

--- migrations/assignments_submissions.sql
+++ migrations/assignments_submissions.sql
@@ -1,3 +1,11 @@
+-- An assignment will have the following attributes:
+--   id: A unique identifier
+--   name: The name of the assignment
+--   content: The written content body of the assignment
+--   day: The day that the assignment appears on
+--   chapter: The order that the assignment will appear in the day.
+--   duration: The average time it takes a student to finish
+
 CREATE TABLE assignments ( id SERIAL PRIMARY KEY NOT NULL,
                            name VARCHAR(255),
                            content TEXT,
@@ -6,6 +14,13 @@ CREATE TABLE assignments ( id SERIAL PRIMARY KEY NOT NULL,
                            duration INTEGER
                           );
 
+
+-- An assignment_submission will have the following attributes:
+--   id: A unique identifier
+--   assignment_id: The id of the assignment
+--   student_id: The id of the student
+--   duration: The time it took the student to complete the assignment
+--   submission_date: The date is was submitted
 
 CREATE TABLE assignment_submissions ( id SERIAL PRIMARY KEY NOT NULL,
                                       assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
@@ -17,17 +32,5 @@ CREATE TABLE assignment_submissions ( id SERIAL PRIMARY KEY NOT NULL,
 
 
 
--- An assignment will have the following attributes:
---   id: A unique identifier
---   name: The name of the assignment
---   content: The written content body of the assignment
---   day: The day that the assignment appears on
---   chapter: The order that the assignment will appear in the day.
---   duration: The average time it takes a student to finish
 
--- An assignment_submission will have the following attributes:
---   id: A unique identifier
---   assignment_id: The id of the assignment
---   student_id: The id of the student
---   duration: The time it took the student to complete the assignment
---   submission_date: The date is was submitted
+
